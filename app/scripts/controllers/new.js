a744b4e37e87ec2c3860d5e6dcf466e596ea43ca
Abinav Seelan <abinav.n.seelan@gmail.com>
2016-02-19 20:00:37 +0530

    Created Function Definitions

--- app/scripts/controllers/new.js
+++ app/scripts/controllers/new.js
@@ -8,14 +8,35 @@
  * Controller of the plutoniumApp
  */
 angular.module('plutoniumApp')
-  .controller('NewCtrl', function ($scope) {
+  .controller('NewCtrl', function ($scope,$http,$q) {
     
   	$scope.data = {
-  		
+
   	}
 
+  	$scope.pauseDeploy = true;
   	$scope.endpointType = function(type){
   		$scope.data.endpointType = type;
+  	}
+
+  	$scope.build = function(){
+  		$scope.pauseDeploy = $scope.sendData();
+  	}
+
+  	$scope.sendData = function(){
+  		var deferred = $q.defer();
+
+  		$http({
+  			method: 'POST',
+  			url: endpoint,
+  			data: $scope.data
+  		}).then(function success(response){
+  			deferred.resolve(true);
+  		}, function error(response){
+  			deferred.resolve(false);
+  		})
+
+  		return deferred.promise;
   	}
 
   });
