e713d84a5f5c43a395afcc49636b257ebd497972
cyberbeast <sandeshgade@gmail.com>
2016-02-20 16:54:14 +0530

    INIT mods

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var bodyParser = require('body-parser');
 
 var port = 8000;
 
+const spawn = require('child_process').spawn;
+
 app.use(cors());
 app.use(bodyParser.json()); 
 
@@ -21,5 +23,18 @@ app.post('/build',function(request,response){
 
 app.post('/init', function(request,response){
 	console.log('Creating Project');
+	const init_spawn = spawn('./scripts/init.sh', ['test_name', 'test_url'])
+
+	init_spawn.stdout.on('data', (data) => {
+		console.log(`stdout: ${data}`);
+	});
+
+	init_spawn.stderr.on('data', (data) => {
+		console.log(`stderr: ${data}`);
+	});
+
+	init_spawn.on('close', (code) => {
+		console.log(`child process exited with code ${code}`);
+	});
 })
 
